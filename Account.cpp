91dfeb5f4b87d574f10d32b1039816653e97c989
yourname30425 <163462701+yourname30425@users.noreply.github.com>
2024-03-25 21:34:12 +0700

    Add files via upload

---
+++ Account.cpp
@@ -0,0 +1,145 @@
+
+#include "Account.h"
+#include "Menu_.h"
+
+void createAccount(vector<Person*>& people) {
+	std::vector<std::string> choice_a = { "User",
+												"Student",
+												"Cancel" };
+	int x = 5, y = 40;
+	int w = 30;
+	int h = 2;
+	showMainMenu(x, y, w, h, choice_a);
+	int choice = (whereY() - y) / 2;
+	if (choice == 0) {
+		Person* ptr = new Person();
+		ptr->input(people);
+		people.push_back(ptr);
+		gotoXY(55, 35);cout << "Successful!" << endl;
+
+	}
+	else if (choice == 1) {
+		Student* ptr = new Student();
+		ptr->input(people);
+		people.push_back(ptr);
+		cout << "Successful!" << endl;
+	}
+	else return;
+	
+}
+
+void asterisk(string& pass) {
+	pass = "";
+	char ch;
+	while (true) {
+		ch = _getch(); // Get a character without echoing it to the console
+
+		if (ch == 13) { // Check if the Enter key is pressed
+			break;
+		}
+		else if (ch == 8 && !pass.empty()) { // Check if Backspace is pressed
+			cout << "\b \b"; // Move the cursor back, print a space, move back again
+			pass.pop_back();
+		}
+		else if (ch == 127 && !pass.empty()) { // Check if Delete is pressed
+			cout << " \b";
+			pass.pop_back();
+		}
+		else if (isprint(ch)) { // Check if the character is printable
+			cout << '*';
+			pass.push_back(ch);
+		}
+	}
+}
+
+void login(vector<Person*>& people) {
+	string u, pass;
+	bool access = false;
+	while (1) {
+		cout << setw(40) << left << "" << "\033[36m----------------------------------------------------------\033[0m" << endl;
+		cout << setw(66) << left << "" << "\033[32mLOG IN\033[0m" << endl;
+		cout << setw(40) << left << "" << "\033[36m----------------------------------------------------------\033[0m" << endl;
+		cout << setw(40) << left << "" << "Username: ";
+		getline(cin, u);
+		cout << setw(40) << left << "" << "Password: ";
+		asterisk(pass);
+		for (int i = 0;i < people.size();i++) {
+			if (people[i]->getAccount() == u && people[i]->getPassword() == pass) {
+				access = true;
+				current = people[i];
+			}
+		}
+		if (access == true) {
+			system("pause");
+			cout << "\033[32mLogged in successfully.\033[0m | Welcome \033[34m" << current->getAccount() << "\033[0m" << endl;
+			cout << "Account type: " << "\033[31m" << current->type << "\033[0m" << endl;
+			cout << endl;
+			return;
+		}
+		else {
+			cout << endl;
+			cout << setw(40) << left << "" << "\033[33mInvalid username or password. Please try again.\033[0m" << endl;
+			cout << setw(40) << left << "" << "\033[36m----------------------------------------------------------\033[0m" << endl;
+		}
+	}
+}
+
+void start(vector<Person*>& people) {
+here:
+	cout << "\033[25;40H" << ".....\033[32mEnter = Log In\033[0m || \033[36mSpace = Create new account\033[0m || \033[33mESC = Exit\033[0m.....";
+	while (1) {
+		if (_kbhit()) {
+			char key = _getch(); // Read a single character
+			if (key == '\r') {
+				// Enter key was pressed
+				system("cls");
+				login(people);
+				break;
+			}
+			else if (key == ' ') {
+				// Space bar was pressed
+				system("cls");
+				cout << "\033[36mCreating new account...\033[0m" << endl;
+				createAccount(people);
+				goto here;
+				break;
+			}
+			else if (key == 27) {
+				writeMangaToFile(list);
+				writePeopleToFile(people);
+				exit(0); // Exit program
+			}
+			else {
+				// Do nothing for other keys
+			}
+		}
+	}
+}
+
+void changePass(int x, int y) {
+	textcolor(7);
+	
+	box(x - 10, y - 3, 50, 10, 11, 1, "\033[31m========CHANGE PASSWORD=======");
+	while (1) {
+		string password, new_password;
+		gotoXY(x, y);
+		std::cout << setw(20) <<left<< "\033[37mPassword: "; getline(cin, password);
+		if (password == current->getPassword()) {
+			gotoXY(x, y + 2);
+			cout << setw(20) <<left<< "\033[37mNew password: ";
+			getline(cin, new_password);
+			current->setPassword(new_password);
+			cout << "\033[37mchange pass word complete.";
+			writePeopleToFile(people);
+			break;
+
+		}
+		else {
+			gotoXY(x, y);
+			cout << " Nhap mat khau sai, vui long nhap lai...";
+
+		}
+	}
+
+}
+
---
+++ Graphic.cpp
@@ -0,0 +1,208 @@
+﻿#include "Graphic.h"
+
+
+
+using namespace std;
+int whereX()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.X;
+	return -1;
+}
+//========= lấy tọa độ y của con trỏ hiện tại =======
+int whereY()
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+		return csbi.dwCursorPosition.Y;
+	return -1;
+}
+//============== dịch con trỏ hiện tại đến điểm có tọa độ (x,y) ==========
+void gotoXY(int x, int y)
+{
+	HANDLE hConsoleOutput;
+	COORD Cursor_an_Pos = { x, y };
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
+//============= đặt màu cho chữ =========
+void SetColor(WORD color)
+{
+	HANDLE hConsoleOutput;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+	GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+	WORD wAttributes = screen_buffer_info.wAttributes;
+	color &= 0x000f;
+	wAttributes &= 0xfff0;
+	wAttributes |= color;
+
+	SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+//============== làm ẩn trỏ chuột ===========
+void ShowCur(bool CursorVisibility)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
+	SetConsoleCursorInfo(handle, &cursor);
+}
+//======= trả về mã phím người dùng bấm =========
+int inputKey()
+{
+	if (_kbhit()) //true
+	{
+		int key = _getch();
+
+		if (key == 224)
+		{
+			key = _getch();
+			return key + 1000;
+		}
+
+		return key;
+	}
+	else
+	{
+		return KEY_NONE;
+	}
+	return KEY_NONE;
+}
+
+void textcolor(int x)
+{
+	HANDLE mau;
+	mau = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(mau, x);
+}
+
+
+
+std::string lowercase(const std::string& str) {
+	std::string result;
+	for (char ch : str) {
+		result += std::tolower(ch);
+	}
+	return result;
+}
+
+
+
+
+
+void thanh_sang(int x, int y, int w, int h, int b_color, std::string  nd)
+{
+	textcolor(b_color);
+	for (int iy = y + 1;iy <= y + h - 1;iy++)
+	{
+		for (int ix = x + 1;ix <= x + w - 1;ix++)
+		{
+			gotoXY(ix, iy);cout << " ";
+		}
+	}
+	SetColor(7);
+	gotoXY(x + (w - nd.length()) / 2, y + 1);
+	cout << nd;
+}
+
+void box(int x, int y, int w, int h, int t_color, int b_color, string nd)
+{
+	textcolor(b_color);
+	for (int iy = y + 1;iy <= y + h - 1;iy++)
+	{
+		for (int ix = x + 1;ix <= x + w - 1;ix++)
+		{
+			gotoXY(ix, iy);cout << " ";
+		}
+	}
+	SetColor(7);
+	gotoXY(x + (w-nd.length())/2, y + 1);
+	cout << nd;
+	//============= ve vien =============
+	textcolor(1);
+	SetColor(t_color);//update
+	if (h <= 1 || w <= 1)return;
+	for (int ix = x;ix <= x + w;ix++)
+	{
+		gotoXY(ix, y);
+		cout << char(196);
+		gotoXY(ix, y + h);
+		cout << char(196);
+	}
+	for (int iy = y;iy <= y + h;iy++)
+	{
+		gotoXY(x, iy);
+		cout << char(179);
+		gotoXY(x + w, iy);
+		cout << char(179);
+	}
+	gotoXY(x, y);cout << char(218);
+	gotoXY(x + w, y);cout << char(191);
+	gotoXY(x, y + h);cout << char(192);
+	gotoXY(x + w, y + h);cout << char(217);
+
+}
+
+
+
+void n_box(int x, int y, int w, int h, int t_color, int b_color,const vector<string>& nds) {
+	int sl = nds.size();
+	for (int i = 0; i < sl; ++i) {
+		box(x, y + (i * 2), w, h, t_color, b_color, nds[i]);
+		if (i != 0) {
+			gotoXY(x, y + (i * 2)); cout << char(195);
+			gotoXY(x + w, y + (i * 2)); cout << char(180);
+		}
+	}
+}
+
+void box_hien_thi_list(int x,int y, int w, int h, int t_color, int b_color, string nds)
+{
+	box(x, y, w, h, t_color, b_color, "");
+	gotoXY(x+3, y+1);
+	std::cout << std::setfill(' ');
+	std::cout << std::setw(2) << std::left << "" << "| ";
+	std::cout << "\033[31m" << std::setw(50) << std::left << "Title";
+	std::cout << "\033[32m" << std::setw(50) << std::left << "Tags";
+	std::cout << "\033[33m" << std::setw(30) << std::left << "Author";
+	std::cout << "\033[34m" << std::setw(15) << std::left << "Date released";
+	std::cout << "\033[35m" << std::setw(5) << std::right << "Rating" << std::endl << "\033[0m";
+	gotoXY(x+3,y +2);
+	std::cout << std::setfill('-') << std::setw(147) << "-" << std::endl;
+}
+
+
+
+void setFullscreen() {
+	HWND hwnd = GetConsoleWindow();
+	ShowWindow(hwnd, SW_MAXIMIZE); // Maximize cửa sổ console
+	ShowWindow(hwnd, SW_SHOWMAXIMIZED); // Hiển thị fullscreen
+}
+
+void clearRectangle(int x1, int y1, int x2, int y2) {
+	COORD topLeft = { x1, y1 };
+	COORD bottomRight = { x2, y2 };
+
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(console, &screen);
+
+	DWORD written;
+	FillConsoleOutputCharacter(console, ' ', (screen.dwSize.X * screen.dwSize.Y), topLeft, &written);
+	FillConsoleOutputAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, (screen.dwSize.X * screen.dwSize.Y), topLeft, &written);
+
+	SetConsoleCursorPosition(console, topLeft);
+}
+
+void clearConsoleLine(const std::string& nds) {
+	ShowCur(0);
+	std::cout << std::setfill(' ');
+	std::cout << "\033[90m" << std::setw(nds.size()) << nds;
+	_getch();
+	std::cout << "\033[" << nds.size() << "D" << std::setw(nds.size()) << " ";
+	std::cout << "\033[" << nds.size() << "D";
+	ShowCur(1);
+}
